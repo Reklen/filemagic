51765e39ec002ab0b9a9e0e0df95c0af9141a454
William Kurosawa <wkurosawa@gmail.com>
2015-10-06 13:28:26 -0300

    Add Refile (and other needed gems) as dependency. Create FormBuilder helper

--- app/helpers/filemagic/application_helper.rb
+++ app/helpers/filemagic/application_helper.rb
@@ -1,4 +1,12 @@
 module Filemagic
   module ApplicationHelper
+    def timeago(time, options = {})
+      options[:class] ||= "timeago"
+      content_tag(
+       :time,
+       time.to_s,
+       options.merge(datetime: time.getutc.iso8601)
+      ) if time
+    end
   end
 end
--- lib/filemagic/engine.rb
+++ lib/filemagic/engine.rb
@@ -1,5 +1,33 @@
+require 'refile/rails'
+
 module Filemagic
   class Engine < ::Rails::Engine
     isolate_namespace Filemagic
+
+    initializer 'filemagic.action_controller' do |app|
+      ActiveSupport.on_load :action_controller do
+        helper Filemagic::ApplicationHelper
+      end
+    end
+  end
+end
+
+class ActionView::Helpers::FormBuilder
+  def filemagic_field(attribute_name, options={})
+
+    preview_size = options[:preview_size] || {}
+    data_attributes = {
+      component: 'Uploader',
+      object: object_name,
+      attribute: attribute_name,
+      preview_size: preview_size,
+      as: "file",
+      url: "/attachments/cache",
+      fields: {},
+      }
+
+    data_attributes = data_attributes.merge(Refile.cache.presign.as_json) if Refile.cache.class.method_defined?(:presign)
+
+    @template.content_tag(:div, nil, { class: 'filemagic-container', data: data_attributes})
   end
 end
